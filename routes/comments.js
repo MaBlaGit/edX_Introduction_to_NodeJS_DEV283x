2c743c32bd746c7a81159eacb630be992642bfbc
Mariusz Blajet <mariuszblajet@gmail.com>
2018-09-22 10:05:45 +0200

    restful api finished

--- routes/comments.js
+++ routes/comments.js
@@ -3,27 +3,56 @@ const store = require('../data');
 module.exports = {
     getComments(req, res) {
       let postId = req.params.postId
-      if(!store.posts[postId].comments){
-        res.send({message: "There is no comment!"})
+      if((store.posts.length - 1) < req.params.postId){
+        res.status(404).send({message: "No post was found!"})
+      }else if(store.posts.length === 0){
+        res.status(404).send({message: "Adding comments impossible, first create post in order to add comments!"})
+      }else if(!store.posts[postId].comments || store.posts[postId].comments.length === 0){
+        res.status(404).send({message: "There is no comments!"})
+      }else{
+        res.status(200).send(store.posts[postId].comments)
       }
-      res.status(200).send(store.posts[postId].comments)
     }, 
 
     addComment(req, res) {
       let postId = req.params.postId
-      if(!store.posts[postId].comments){  
-        store.posts[postId].comments = []
-      }
-      store.posts[postId].comments.push({"text": req.body.comments})
-      res.status(200).send({message: `Comment was added successfully`})
-    },
 
+      if((store.posts.length - 1) < postId){
+        res.status(404).send({message: "Adding comments impossible, no post found"})
+      }else if(store.posts.length === 0){
+        res.status(404).send({message: "Adding comments impossible, first create post in order to add comments!"})
+      }else if(!store.posts[postId].comments && req.body.comments){  
+        store.posts[postId].comments = []
+        store.posts[postId].comments.push({"text": req.body.comments})
+        res.status(200).send({message: "Comment was added successfully"})
+      }else if(!req.body.comments){
+        res.send({message: "Lack of comment in the payload!"})
+      } else {
+        store.posts[postId].comments.push({"text": req.body.comments})
+        res.status(200).send({message: "Comment was added successfully"})
+      }
+    },
     updateComment(req, res) {
-      
-      
+      let postId = req.params.postId
+      let commentId = req.params.commentId
+      if((store.posts.length - 1) < req.params.postId || (store.posts[postId].comments.length - 1) < req.params.commentId){
+        res.status(404).send({message: "Comment or post where comment should be not found!"})
+      }else{
+        store.posts[postId].comments[commentId].text = req.body.comments
+        res.status(200).send({message: "Comment updated"})
+      }
     },
 
     removeComment(req, res) {
-      
+      let postId = req.params.postId
+      let commentId = req.params.commentId
+      if((store.posts.length - 1) < req.params.postId || (store.posts[postId].comments.length - 1) < req.params.commentId){
+        res.status(404).send("Comment do not exists!")
+      }else if(!store.posts[postId].comments){
+        res.status(404).send("There is no comment to delete")
+      }else{
+        store.posts[postId].comments.splice(req.params.commentId, 1)
+        res.status(200).send({message: "Comment was removed successfully!"})
+      }
     }  
   }
--- routes/post.js
+++ routes/post.js
@@ -2,23 +2,35 @@ const store = require('../data');
 
 module.exports = {
     getPosts(req, res) {
-        res.status(200).send(store.posts)
+        if(store.posts.length === 0){
+            res.status(200).send({message: "No posts were found, add some post first!"})
+        }else{
+            res.status(200).send(store.posts)
+        }
     },
 
     addPost(req, res) {
         let newPost = req.body
         store.posts.push(newPost)
-        res.status(201).send(newPost)
+        res.status(201).send({message: "New post added successfully!"})
     },
 
     updatePost(req, res) {
-        store.posts[req.params.postId] = req.body
-        res.status(200).send(store.posts[req.params.postId])
+        if((store.posts.length - 1) < req.params.postId){
+            res.status(404).send({message: "Selected post to update not found!"})
+        }else{
+            store.posts[req.params.postId] = req.body
+            res.status(200).send({message: "Selected post updated successfully!"})
+        }
     },
 
     removePost(req, res) {
+        if((store.posts.length - 1) < req.params.postId){
+            res.status(404).send({message: "Post to delete not found!"})
+        }else{
         store.posts.splice(req.params.postId, 1)
-        res.status(204).send()
+        res.status(200).send({message: "Selected post deleted premanently!"})
+        }
     }
   }
   
